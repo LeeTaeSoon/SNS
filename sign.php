8826b3ea72bbfa4e65e90060f0ea05ba7d164326
LeeTaeSoon <nrst136@naver.com>
2016-05-22 23:44:56 +0900

    회원가입 페이지 파일

---
+++ sign.php
@@ -0,0 +1,29 @@
+<?
+	include("function.php");
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title> See Saw </title>
+		<link rel="stylesheet" type="text/css" href="sign.css">
+	</head>
+
+	<body>
+		
+		<div id="page-wrapper">
+
+			<div class="sign-page">
+
+				<form action="" method="post">
+
+				</form>
+			</div>
+
+		</div>
+	
+	</body>
+
+</html>
