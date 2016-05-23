19b5d5d023c9df6752e21237e77470ed7847aee6
LeeTaeSoon <nrst136@naver.com>
2016-05-23 23:51:50 +0900

    메뉴바 파일
    메뉴바 생성

---
+++ menubar.php
@@ -0,0 +1,18 @@
+<div class="menu-bar">
+	<div class="logo">
+	</div>
+
+	<div class="user-menu">
+		<div class="user-name">
+			<span class="name">이태순</span> 님
+		</div>
+
+		<div class="logout-button">
+			<a href="logout.php">로그아웃</a>	
+		</div>
+	</div>
+
+	<div class="all-menu-button">
+		<img src="image/menu-icon.png" class="menu-icon">
+	</div>
+</div>
