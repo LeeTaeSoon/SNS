b0048cbc7da96f2e177b93f212de4417173bd6ae
LeeTaeSoon <nrst136@naver.com>
2016-05-28 01:04:56 +0900

    영화 목록 페이지 레이아웃

---
+++ movie_list.php
@@ -0,0 +1,48 @@
+<?
+	if(!isset($_SESSION))
+		session_start();
+
+	$id = $_SESSION["id"];
+	$name = $_SESSION["name"];
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title> See Saw </title>
+
+	<link rel="stylesheet" type="text/css" href="init-style.css">
+	<link rel="stylesheet" type="text/css" href="menubar.css">
+	<link rel="stylesheet" type="text/css" href="movie_list.css">
+</head>
+
+<body>
+<?	
+	include("menubar.php");
+?>
+	<div id="page-wrapper">
+<?
+	for($i = 0 ; $i < 20 ; $i++)
+	{
+?>
+		<div class="movie">
+			<div class="poster">
+				<img src="image/menu-icon - 원본.png" class="poster-img">
+			</div>
+
+			<!-- <div class="star-score">
+				
+			</div> -->
+
+			<div class="movie-name">
+				주토피아
+			</div>
+		</div>
+<?
+	}
+?>		
+	</div>
+
+</body>
+</html>
