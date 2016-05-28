6b4816e50e5ee9db700dd3fcdc7172d1f71b13a9
sh-y <glgldf12@naver.com>
2016-05-28 14:39:08 +0900

    글 보여주는 페이지 추가, 댓글 추가

---
+++ add_comment.php
@@ -0,0 +1,13 @@
+<?php
+/*
+$db = new PDO("mysql:dbname=reply;host=localhost","root","apmsetup");	
+
+$re = $db->quote($_GET["comment"]);
+
+$query = "insert into reply(replys)";
+$query .= "values($re)";
+
+$result = $db->exec($query);
+
+header("Location:show.php");*/
+?>
---
+++ show.php
@@ -0,0 +1,82 @@
+<?
+	if(!isset($_SESSION))
+		session_start();
+
+	$id = $_SESSION["id"];
+	$name = $_SESSION["name"];
+
+	$db = new PDO("mysql:dbname=reply;host=localhost","root","apmsetup");
+
+?>
+
+<html>
+
+<head>
+
+	<link rel="stylesheet" type="text/css" href="init-style.css">
+	<link rel="stylesheet" type="text/css" href="film_div.css">
+	<link rel="stylesheet" type="text/css" href="menubar.css">
+	<link rel="stylesheet" type="text/css" href="show.css">
+</head>
+
+
+<body>
+
+<div id="top">
+ 	<? include("menubar.php") ?>
+</div>
+
+
+<div id ="pagewrapper" >
+	<? include("film_div.php") ?>
+	
+	<div id= "go-left">
+		<a href ="#"><img src="left.png" width="80" height="80" ></a>
+	</div>
+	
+	<div id ="content">
+		<div id ="article">
+		
+	<!-- 	try{$db = new PDO("mysql:dbname=db_ip;host=localhost","root","apmsetup");
+ 	 			$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 	 	
+ -->
+		</div>
+
+		<div id = "comment">
+			<div id="show_comment">
+				<?	$query = "select*from reply";
+					$rows = $db->query($query);
+					
+					$n = $rows->rowCount();
+					
+					for($i=0; $i < $n ; $i = $i+1)
+					{	
+						$row = $rows->fetch();
+						$comment = $row[1];
+
+					?>
+					<table> 
+						<tr>
+							<th> 작성자 </th>
+						</tr>
+						<tr>
+							<td><? echo $comment?></td>
+						</tr>
+					</table>
+				<?	
+					} 
+				?>
+			</div>
+			</hr>
+			<div id = plus_comment>
+				<form action="add_comment.php" method="GET">
+					<input type="text" name="comment" value="댓글을 달아 보세요">
+					<input type="submit" value="입력"> 
+				</form>
+			</div>	
+		</div>
+
+	</div>
+	<? include("film_div.php") ?>
+</div>
+</body>
