9227d3a678b42b32a6ee5352e48df0a282891b86
sh-c <seunghee.c67@gmail.com>
2016-06-19 03:41:21 +0900

    movie_name

---
+++ autocomplete.php
@@ -0,0 +1,22 @@
+
+<script type="text/javascript" src="jquery/lib/jquery.js"></script>
+<script type='text/javascript' src='jquery/lib/jquery.bgiframe.min.js'></script>
+<script type='text/javascript' src='jquery/lib/jquery.ajaxQueue.js'></script>
+<script type='text/javascript' src='jquery/jquery.autocomplete.js'></script>
+<link rel="stylesheet" type="text/css" href="jquery/jquery.autocomplete.css" />
+
+	<script>
+	var availableTags = [ <? include("movie_names.txt"); ?> ];
+	</script>
+
+	<input type="text" id="searchbox" id="auto" >
+	
+
+	<script>
+	$(document).ready(function() {
+	    $("#searchbox").autocomplete(availableTags,{ 
+	        matchContains: true,
+	        selectFirst: false
+	    });
+	});
+	</script>
